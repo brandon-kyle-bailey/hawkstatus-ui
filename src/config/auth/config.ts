e4d9787ecb5885353913dff5db07e653a5926b58
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2023-08-28 03:07:07 -0300

    Initial commit

---
+++ src/config/auth/config.ts
@@ -0,0 +1,66 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  debug: true,
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+    newUser: "/auth/new-user",
+  },
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        console.log(credentials);
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log("fire signin Callback");
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      console.log("fire redirect Callback");
+      return baseUrl;
+    },
+    async session({ session, user, token }) {
+      console.log("fire SESSION Callback");
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      console.log("fire jwt Callback");
+
+      // console.log({ token, user, account, profile, isNewUser });
+      return token;
+    },
+  },
+};
